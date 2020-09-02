becc22394bc85501acb0b15256b5d65917f359e5
aiyingyi <mt1309961163@163.com>
2020-09-02 18:54:31 +0800

    统计地图完成

--- src/main/resources/统计地图.sh
+++ src/main/resources/统计地图.sh
@@ -6,11 +6,9 @@ do_date=`date  "+%Y-%m-%d %H:%M:%S"`
 start_time=`date -d "6 hour ago  ${do_date}" "+%Y-%m-%d %H:%M:%S"`
 
 sql="
-select
-
-
-from
-    (select
+with
+warning as
+(select
       province,
       risk_level,
       count(*) total
@@ -18,9 +16,26 @@ from
     where date_format(warning_start_time,'yyyy-MM-dd HH') >= date_format('${start_time}','yyyy-MM-dd HH')
     and date_format(warning_start_time,'yyyy-MM-dd HH') < date_format('${do_date}','yyyy-MM-dd HH')
     and review_status >= '1'
-    and review_status >= '3') warning
-group by
-;
+    and review_status >= '3'
+    group by province,risk_level ),
+high_risk as
+(select province,total from warning where risk_level = '1'),
+med_risk as
+(select province,total from warning where risk_level = '2'),
+low_risk as
+(select province,total from warning where risk_level = '3')
+
+insert into table province_warning_statistic_es
+select
+  high_risk.province,
+  high_risk.total,
+  med_risk.total,
+  low_risk.total,
+  0,
+  date_format('${start_time}','yyyy-MM-dd HH')
+from
+  high_risk join  med_risk on med_risk.province = high_risk.province
+join low_risk on high_risk.province = low_risk.province;
 "
 hive -e  "${sql}"
 
